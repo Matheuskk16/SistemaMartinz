c49ac990ddc87a0898d0a5964f2c76f8ee879b64
Matheus Vinicius LeÃo Martinz MARTINZ <u09012456150@acad.pp.ifms.edu.br>
2026-04-07 15:16:08 -0400

    Criamos o BEAN e o DAO e a tabela de usuarios no bean

---
+++ src/bean/MpvUsuarios.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package bean;
+
+import java.util.Date;
+
+/**
+ *
+ * @author u09012456150
+ */
+public class MpvUsuarios {
+    int mpv_IdUsuarios;
+    String mpv_Nome;
+    String mpv_Apelido;
+    String mpv_Cpf;
+    Date mpv_DataNascimento;
+    int mpv_Nivel;
+    String mpv_Senha;
+    String mpv_Ativo;
+}
