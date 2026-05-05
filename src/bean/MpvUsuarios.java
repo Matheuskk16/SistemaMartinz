43e487fd304d8a1c0c382d473d4a8834c1bf10dc
Cheshire-dev <leonardobang123carvalho@gmail.com>
2026-05-05 15:03:59 -0400

    Ultima aula

--- src/bean/MpvUsuarios.java
+++ src/bean/MpvUsuarios.java
@@ -11,12 +11,125 @@ import java.util.Date;
  * @author u09012456150
  */
 public class MpvUsuarios {
-    int mpv_IdUsuarios;
-    String mpv_Nome;
-    String mpv_Apelido;
-    String mpv_Cpf;
-    Date mpv_DataNascimento;
-    int mpv_Nivel;
-    String mpv_Senha;
-    String mpv_Ativo;
+    private int mpv_IdUsuarios;
+    private String mpv_Nome;
+    private String mpv_Apelido;
+    private String mpv_Cpf;
+    private Date mpv_DataNascimento;
+    private int mpv_Nivel;
+    private String mpv_Senha;
+    private String mpv_Ativo;
+
+    /**
+     * @return the mpv_IdUsuarios
+     */
+    public int getMpv_IdUsuarios() {
+        return mpv_IdUsuarios;
+    }
+
+    /**
+     * @param mpv_IdUsuarios the mpv_IdUsuarios to set
+     */
+    public void setMpv_IdUsuarios(int mpv_IdUsuarios) {
+        this.mpv_IdUsuarios = mpv_IdUsuarios;
+    }
+
+    /**
+     * @return the mpv_Nome
+     */
+    public String getMpv_Nome() {
+        return mpv_Nome;
+    }
+
+    /**
+     * @param mpv_Nome the mpv_Nome to set
+     */
+    public void setMpv_Nome(String mpv_Nome) {
+        this.mpv_Nome = mpv_Nome;
+    }
+
+    /**
+     * @return the mpv_Apelido
+     */
+    public String getMpv_Apelido() {
+        return mpv_Apelido;
+    }
+
+    /**
+     * @param mpv_Apelido the mpv_Apelido to set
+     */
+    public void setMpv_Apelido(String mpv_Apelido) {
+        this.mpv_Apelido = mpv_Apelido;
+    }
+
+    /**
+     * @return the mpv_Cpf
+     */
+    public String getMpv_Cpf() {
+        return mpv_Cpf;
+    }
+
+    /**
+     * @param mpv_Cpf the mpv_Cpf to set
+     */
+    public void setMpv_Cpf(String mpv_Cpf) {
+        this.mpv_Cpf = mpv_Cpf;
+    }
+
+    /**
+     * @return the mpv_DataNascimento
+     */
+    public Date getMpv_DataNascimento() {
+        return mpv_DataNascimento;
+    }
+
+    /**
+     * @param mpv_DataNascimento the mpv_DataNascimento to set
+     */
+    public void setMpv_DataNascimento(Date mpv_DataNascimento) {
+        this.mpv_DataNascimento = mpv_DataNascimento;
+    }
+
+    /**
+     * @return the mpv_Nivel
+     */
+    public int getMpv_Nivel() {
+        return mpv_Nivel;
+    }
+
+    /**
+     * @param mpv_Nivel the mpv_Nivel to set
+     */
+    public void setMpv_Nivel(int mpv_Nivel) {
+        this.mpv_Nivel = mpv_Nivel;
+    }
+
+    /**
+     * @return the mpv_Senha
+     */
+    public String getMpv_Senha() {
+        return mpv_Senha;
+    }
+
+    /**
+     * @param mpv_Senha the mpv_Senha to set
+     */
+    public void setMpv_Senha(String mpv_Senha) {
+        this.mpv_Senha = mpv_Senha;
+    }
+
+    /**
+     * @return the mpv_Ativo
+     */
+    public String getMpv_Ativo() {
+        return mpv_Ativo;
+    }
+
+    /**
+     * @param mpv_Ativo the mpv_Ativo to set
+     */
+    public void setMpv_Ativo(String mpv_Ativo) {
+        this.mpv_Ativo = mpv_Ativo;
+    }
+    
 }
